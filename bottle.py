5d672ee78d4cda26616006b9b55bf424e4e71991
wzhiyuan1212 <wzhiyuan1212@gmail.com>
2015-05-06 21:40:38 +0800

    add bottle source code

---
+++ bottle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'bottle==0.12.8','bottle.py'
+__requires__ = 'bottle==0.12.8'
+import pkg_resources
+pkg_resources.run_script('bottle==0.12.8', 'bottle.py')
